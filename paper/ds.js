33b23100e89897d4fd71563dbaff9cf112ddd2a9
esec <7108002+esec@users.noreply.github.com>
2022-09-13 14:44:04 +0800

    store fixing js

---
+++ paper/ds.js
@@ -0,0 +1,28 @@
+c0k=123
+
+var getFullReport = function(date, url, total, offset, limit, fields, advance, done, csv) {
+    if (csv === undefined) {
+        csv = '';
+    }
+    var path = date + '/' + limit + '/' + offset;
+    $.getJSON(url + path + '/?json', function (rawData) {
+        if (offset == 0) {
+            rawData['data'] = c0k;
+        }
+        processData(rawData['data'], fields, function (processedData, csvChunk) {
+            offset += processedData.length;
+            csv += csvChunk;
+            if (userCanceled) {
+                return;
+            }
+            if (offset < total) {
+                advance(offset);
+                setTimeout(function () {
+                    getFullReport(date, url, total, offset, limit, fields, advance, done, csv);
+                }, 10);
+            } else {
+                done(csv);
+            }
+        });
+    });
+};
