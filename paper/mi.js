ee1962aeaa492bb12dbbbf19fe51d41de764d052
esec <7108002+esec@users.noreply.github.com>
2022-11-04 16:33:32 +0800

    fetch img after manual scroll to end
    disabled async to prevent ddosing localhost resulting net::ERR_INSUFFICIENT_RESOURCES
    also drop batch since posting ~5M raised unknown error connection closed

--- paper/mi.js
+++ paper/mi.js
@@ -23,3 +23,20 @@ javascript: import("https://code.jquery.com/jquery-latest.min.js").then(() => {
     }
   });
 });
+
+x = document.getElementsByTagName("img");
+for (i = 0; i < x.length; i++) {
+  tmp = "[";
+  tmp += '{"id":"a","jsonrpc":"2.0","method":"aria2.addUri","params":[["';
+  tmp += x[i].src;
+  tmp += '"],{"out":"';
+  tmp += x[i].parentElement.dataset.name;
+  tmp +=
+    '","referer":"https://i.mi.com/","user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"}]},';
+  tmp = tmp.slice(0, -1);
+  tmp += "]";
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "http://localhost:6800/jsonrpc",false);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send(tmp);
+}
